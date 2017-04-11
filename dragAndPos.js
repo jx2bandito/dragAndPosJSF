eb422688bef7083ea5d5a7c800830232baaf5439
Jose Jacob Aquino Abubo <jx2abubo@gmail.com>
2017-04-11 16:23:03 -0700

    show hundredth decimal

--- dragAndPos.js
+++ dragAndPos.js
@@ -3,8 +3,8 @@ function dragAndPos($movable, $output) {
   $movable.draggable({
     drag: function() {
 
-      var percentLeft = Math.round($(this).position().left / $(this).parent().width() * 1000) / 10 + "%";
-      var percentRight = Math.round($(this).position().top / $(this).parent().height() * 1000) / 10 + "%";
+      var percentLeft = Math.round($(this).position().left / $(this).parent().width() * 10000) / 100 + "%";
+      var percentRight = Math.round($(this).position().top / $(this).parent().height() * 10000) / 100 + "%";
 
       $output.html("#" + this.id + "{<br>&nbsp;&nbsp;position: absolute;<br>" +
         "&nbsp;&nbsp;left: " +
